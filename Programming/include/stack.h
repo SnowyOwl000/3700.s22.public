393f041864e77d8dbf5f591a902c6f638afeef71
Bob K <rwkramer@ysu.edu>
2022-03-04 12:43:50 -0500

    added lab 8

--- Programming/include/stack.h
+++ Programming/include/stack.h
@@ -1,60 +1,64 @@
 #ifndef _STACK_H
 #define _STACK_H
+
 #include <stdexcept>
+
 using namespace std;
 
-const int32_t
-  STACK_SIZE = 64;
+const uint32_t
+    STACK_SIZE = 16;
   
 template <typename StackType>
 class Stack {
- public:
-  Stack(int32_t sSize=STACK_SIZE) {
+public:
+    Stack(uint32_t sSize=STACK_SIZE) {
   
-    data = new StackType[sSize];
-    capacity = sSize;
+        data = new StackType[sSize];
+        capacity = sSize;
     
-    top = 0;
-  }
+        top = 0;
+    }
+
+    ~Stack() { delete[] data; }
+
+    void clear() { top = 0; }
+
+    uint32_t size() { return top; }
+
+    bool isEmpty() { return !top; }
   
-  ~Stack() { delete[] data; }
+
+    void push(StackType d) {
   
-  void clear() { top = 0; }
-  
-  int32_t size() { return top; }
-  bool isEmpty() { return !top; }
-  
-  void push(StackType d) {
-  
-    if (top == capacity)
-      throw overflow_error("Stack is full");
+        if (top == capacity)
+            throw overflow_error("Stack is full");
       
-    data[top++] = d;
-  }
+        data[top++] = d;
+    }
   
-  StackType pop() {
-  
-    if (top == 0)
-      throw underflow_error("Stack is empty");
+    StackType pop() {
+
+        if (top == 0)
+            throw underflow_error("Stack is empty");
       
-    return data[--top];
-  }
+        return data[--top];
+    }
     
-  StackType peek() {
+    StackType peek() {
   
-    if (top == 0)
-      throw underflow_error("Stack is empty");
+        if (top == 0)
+            throw underflow_error("Stack is empty");
       
-    return data[top-1];
-  }
+        return data[top-1];
+    }
   
- private:
-  StackType
-    *data;
+private:
+    StackType
+        *data;
     
-  int32_t
-    top,
-    capacity;
+    uint32_t
+        top,
+        capacity;
 };
 
 #endif
